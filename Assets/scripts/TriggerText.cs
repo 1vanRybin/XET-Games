f5312230fab8a65592c0ee4f73b70954478a9888
Ivan <117839645+1vanRybin@users.noreply.github.com>
2023-03-31 18:36:02 +0500

    remake UI.

--- Assets/scripts/TriggerText.cs
+++ Assets/scripts/TriggerText.cs
@@ -1,27 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TriggerText : MonoBehaviour
 {
-    public Text triggerText;
-    public Text counter;
-    int count;
+    [SerializeField] private Text chargeText;
+    [SerializeField] private Text chargeLevel;
+    public static int count;
+
+    private void Start()
+    {
+        chargeText.enabled = false;
+    }
+
+    private void Update()
+    {
+        chargeLevel.color = (count <= 10) ?
+             Color.red: Color.green;
+        chargeLevel.text = count.ToString();
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
-            triggerText.text = "Van Sama here!!!";
-            count++;
-            counter.text = count.ToString();
+            chargeText.enabled = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E) && count<100)
+                count++;
         }
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
         if (col.tag == "Player")
-            triggerText.text = "";
+            chargeText.enabled = false;
     }
+
 }
